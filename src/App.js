7d8347b6a8c4bf7fd808ddd6b01cadf2bfdca8ca
JWsteel <72229305+JWsteel@users.noreply.github.com>
2022-12-01 11:09:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CollectionForm from './Form'
 
 class App extends Component{
 
-    API_URL="http://193.191.176.22:8080"
+    API_URL="http://193.191.176.22:8088"
 
     state = {
       products: [],
